fb4844e093d4e0bde16766acfadc75baa2f21885
IYahirMP <ivanyahirmopi@gmail.com>
2024-07-29 18:08:06 -0600

    Galeria carga datos por API

--- src/Pages/Gallery/Gallery.jsx
+++ src/Pages/Gallery/Gallery.jsx
@@ -1,24 +1,28 @@
 import { useTheme } from "@mui/material"
 import {Container, Box, Typography} from "@mui/material";
 import GalleryCard from "../../Components/GalleryCard/GalleryCard";
+import { useQuery } from "@tanstack/react-query";
 
 export default function Gallery (){
     const theme = useTheme();
 
-    const titleGarden = 'Jardin de Polinizadores del Instituto Tecnológico de Ciudad Altamirano';
+    const {isPending, error, data} = useQuery(
+        {
+            queryKey: ['galleryData'],
+            queryFn: () => fetch('http://127.0.0.1:8000/species').then((res) => res.json(),),
+        }
+    )
 
+    const titleGarden = 'Jardin de Polinizadores del Instituto Tecnológico de Ciudad Altamirano';
     return (
     <Container maxWidth='xlg' disableGutters>
         <Box {...theme.gallery.presentation}>
             <Typography variant='h2'>{titleGarden}</Typography>
         </Box>
         <Box {...theme.gallery.cardBox}>
-            <GalleryCard title={'Lepidoptera'} description={'Some lepidoptera'} img={'http://127.0.0.1/species/Lepidoptera.jpg'} />
-            <GalleryCard title={'Lepidoptera'} description={'Some lepidoptera'} img={'http://127.0.0.1/species/Lepidoptera.jpg'} />
-            <GalleryCard title={'Lepidoptera'} description={'Some lepidoptera'} img={'http://127.0.0.1/species/Lepidoptera.jpg'} />
-            <GalleryCard title={'Lepidoptera'} description={'Some lepidoptera'} img={'http://127.0.0.1/species/Lepidoptera.jpg'} />
-            <GalleryCard title={'Lepidoptera'} description={'Some lepidoptera'} img={'http://127.0.0.1/species/Lepidoptera.jpg'} />
-            <GalleryCard title={'Lepidoptera'} description={'Some lepidoptera'} img={'http://127.0.0.1/species/Lepidoptera.jpg'} />
+            {isPending && <Typography variant="h2">Aun no hay datos.</Typography>}
+            {error && <Typography variant="h2">Ha ocurrido un error al solicitar los datos del servidor.</Typography>}
+            {data != undefined && error != true && data.map((card) => <GalleryCard key={card.id} title={card.species} description={card.description} img={card.img}/>)}
         </Box>
     </Container>
     );
--- src/main.jsx
+++ src/main.jsx
@@ -15,6 +15,14 @@ import {
   createBrowserRouter,
   RouterProvider,
 } from 'react-router-dom'
+
+import{
+  QueryClient,
+  QueryClientProvider,
+} from '@tanstack/react-query';
+
+import {ReactQueryDevtools} from '@tanstack/react-query-devtools';
+
 import Gallery from './Pages/Gallery/Gallery.jsx'
 import { ThemeProvider } from '@emotion/react'
 
@@ -39,10 +47,15 @@ const router = createBrowserRouter([
   },
 ])
 
+const queryClient = new QueryClient();
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <ThemeProvider theme={theme}>
-      <RouterProvider router={router} />
-    </ThemeProvider>
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider theme={theme}>
+        <RouterProvider router={router} />
+      </ThemeProvider>
+      <ReactQueryDevtools/>
+    </QueryClientProvider>
   </React.StrictMode>,
 )
